2991f6f0f1488ab27df9b759acbfefe619632c01
Norman Davie <ecompservices@gmail.com>
2023-11-22 15:48:56 -0400

    Add 3D audio

--- VR_test/Scenes/main.gd
+++ VR_test/Scenes/main.gd
@@ -1,19 +1,19 @@
 extends Node3D
 
+
 var xr_interface : XRInterface
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
-
+	
 	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR initialized successfully.")
+		print("OpenXR initialized successfully")
 		
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		
 		get_viewport().use_xr = true
 		
 	else:
-		print("OpenXR not initialized.")
-		print("Confirm VR headset is connected.")
+		print("Failed to initialized. Please check to see if the headset is connected.")
 		
 		
